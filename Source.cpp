a67d1cd9e93fd670510443eabcd2c59944ccf012
Laert-Mkrtumyan <63158093+Laert-Mkrtumyan@users.noreply.github.com>
2020-04-04 20:23:27 +0400

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,54 +1,69 @@
 #include <iostream>
 #include <vector>
+#define Parent(i)(i / 2)
+#define L(i)(2 * i + 1)
+#define R(i)(2 * i + 2)
 using namespace std;
-int Hoare_Partition(vector<int>& v, int l, int r)
+void Max_Heapify(vector<int>& v, int i, int heap_size)
 {
-	int pivot = v[l];
-	int i = l - 1, j = r + 1;
-	while (1)
-	{
-		do
-		{
-			--j;
-		} while (v[j] <= pivot);
-		do
-		{
-			++i;
-		} while (v[i] > pivot);
-		if (i >= j)
-			return j;
-		swap(v[i], v[j]);
-	}
+    int largest = i;
+    if (L(i) <= heap_size && v[L(i)] > v[largest])
+        largest = L(i);
+    if (R(i) <= heap_size && v[R(i)] > v[largest])
+        largest = R(i);
+    if (largest != i)
+    {
+        swap(v[i], v[largest]);
+        Max_Heapify(v, largest, heap_size);
+    }
 }
-int Partiton(vector<int>& v, int l, int r)
+void Build_Max_Heap(vector<int>& v, int heap_size) 
 {
-	int pivot = v[r];
-	int last_small_value_index = l - 1;
-	for (int i = l; i < r; i++)
-	{
-		if (pivot >= v[i])
-			swap(v[++last_small_value_index], v[i]);
-	}
-	swap(v[last_small_value_index + 1], v[r]);
-	return last_small_value_index + 1;
+    for (int i = v.size() / 2 - 1; i >= 0; i--)
+        Max_Heapify(v, i, heap_size);
 }
-int Randomize_Partition(vector<int>& v, int l, int r)
+void HeapSort(vector<int>& v) // We sort all array
 {
-	int random_index = rand() % (r - l) + l;
-	swap(v[r], v[random_index]);
-	return Partiton(v, l, r);
+    int heap_size = v.size() - 1;
+    Build_Max_Heap(v, heap_size);
+    for (int i = v.size() - 1; i >= 1; i--)
+    {
+        swap(v[0], v[i]);
+        --heap_size;
+        Max_Heapify(v, 0, heap_size);
+    }
 }
-void Quick_Sort(vector<int>& v, int l, int r)
+void Heap_Extract_Max(vector<int>& v) // We remove max element
 {
-	if (l >= r) return;
-	int index = Partiton(v, l, r); // int index = Randomize_Partition(v, l, r);
-	Quick_Sort(v, l, index - 1);
-	Quick_Sort(v, index + 1, r);
+    int heap_size = v.size() - 1;
+    if (heap_size < 0)
+        return;
+    swap(v[0], v[heap_size]);
+    v.pop_back();
+    --heap_size;
+    Max_Heapify(v, 0, heap_size);
+}
+void Heap_Increase_Key(vector<int>& v, int i, int key) // We change the value of given index if and only if value is greater
+{
+    if (v[i] > key)
+        return;
+    v[i] = key;
+    while (i > 0 && v[i] > v[Parent(i)])
+    {
+        swap(v[i], v[Parent(i)]);
+        i = Parent(i);
+    }
+}
+void Max_Heap_Insert(vector<int>& v, int key) // We insert new key
+{
+    v.push_back(INT_MIN);
+    Heap_Increase_Key(v, v.size() - 1 ,key);
 }
 int main()
 {
-	vector<int> v{ -1, 126, 130, 999, -1561, 0 };
-	Quick_Sort(v, 0, v.size() - 1);
-	for (int e : v)
-		cout << e << " ";
+    vector<int> v{ 16, 14, 10, 8, 1214545, 9, 3, 2, 4, 1 };
+    Build_Max_Heap(v, v.size() - 1);
+    for (int a : v)
+        cout << a << " ";
+
 }
